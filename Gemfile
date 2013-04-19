d57911057a84eab86e5823d9592248028e0f950a
aqingsao <xqzhang@thoughtworks.com>
2013-04-19 15:20:12 +0800

    Introduce data_mapper as ORM

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 gem 'sinatra', :github => "sinatra/sinatra"
-
+gem 'data_mapper'
+gem 'dm-core'
+gem 'dm-sqlite-adapter'
+gem 'dm-migrations'
 gem 'slim'
 
 group :test do
--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,29 @@
 require 'rubygems'
 require 'bundler/setup'
-
 require 'sinatra/base'
+require 'dm-core'
 require 'slim'
+
+configure :development do
+  DataMapper::Logger.new($stdout, :debug)
+  DataMapper.setup(:default, 'sqlite::memory:')
+end
+
+configure :production do
+  DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{File.expand_path(File.dirname(__FILE__))}/#{Sinatra::Base.environment}.db"))
+  # DataMapper.setup(:default, 'mysql://user:password@hostname/database')
+
+end
+
+class Todo
+  include DataMapper::Resource
+  property :id,         Serial
+  property :title,      String
+  property :created_at, DateTime
+  property :complete,   Boolean, :default=>false
+
+  validates_present :title
+end
 
 class Application < Sinatra::Base
   set :foo, 'bar'
@@ -13,4 +34,11 @@ class Application < Sinatra::Base
   get '/hi' do
     'Hi'
   end
+  get '/todos' do
+  	@todos=Todo.all :order=>[:created_at]
+  	slim :index
+  end
 end
+
+# class TodoController < Sinatra::Base
+# end
